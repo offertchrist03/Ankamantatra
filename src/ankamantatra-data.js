96222de5b37b61c638b730fdecbdc5426583de9e
Christ-Offert <142915490+offertchrist03@users.noreply.github.com>
2024-12-17 01:02:13 +1300

    Update ankamantatra-data.js

--- src/ankamantatra-data.js
+++ src/ankamantatra-data.js
@@ -2,55 +2,515 @@
 
 const data = [
   {
-    id: `ankamantatra-0`,
-    title: `Loharano an-tendron-kazo`,
-    question: [
-      { text: `voasary`, answer: 1 },
-      { text: `apongabe an'danitra`, answer: 0 },
-      { text: `paiso`, answer: 0 },
-      { text: `pibasy`, answer: 0 },
-    ],
+    "id": "ankamantatra-0",
+    "title": "Loharano an-tendron-kazo",
+    "question": [
+      { "text": "voasary", "answer": 1 },
+      { "text": "apongabe an'danitra", "answer": 0 },
+      { "text": "paiso", "answer": 0 },
+      { "text": "pibasy", "answer": 0 }
+    ]
   },
   {
-    id: `ankamantatra-1`,
-    title: `Ilay bota mitaingin-tseza`,
-    question: [
-      { text: `mananasy`, answer: 1 },
-      { text: `kotobekibo`, answer: 0 },
-      { text: `paiso`, answer: 0 },
-      { text: `zavoka`, answer: 0 },
-    ],
+    "id": "ankamantatra-1",
+    "title": "Iza no tsy miasa fa miteraka?",
+    "question": [
+      { "text": "rain'ny vehivavy", "answer": 0 },
+      { "text": "ny aretina", "answer": 0 },
+      { "text": "ny fo", "answer": 0 },
+      { "text": "ny marary", "answer": 1 }
+    ]
   },
   {
-    id: `ankamantatra-2`,
-    title: `Oano aho inanako anao`,
-    question: [
-      { text: `sakay`, answer: 1 },
-      { text: `afo`, answer: 0 },
-      { text: `"courant"`, answer: 0 },
-      { text: `liona`, answer: 0 },
-    ],
+    "id": "ankamantatra-2",
+    "title": "Inona no mandeha, fa tsy mihetsika?",
+    "question": [
+      { "text": "ny fotoana", "answer": 1 },
+      { "text": "ny rivotra", "answer": 0 },
+      { "text": "ny tany", "answer": 0 },
+      { "text": "ny rano", "answer": 0 }
+    ]
   },
   {
-    id: `ankamantatra-3`,
-    title: `Bibilava mifoka sigara`,
-    question: [
-      { text: `lamasinina`, answer: 1 },
-      { text: `bibilava`, answer: 0 },
-      { text: `lakozia`, answer: 0 },
-      { text: `fiarakodia`, answer: 0 },
-    ],
+    "id": "ankamantatra-3",
+    "title": "Inona no mitombo nefa tsy mihinjitra?",
+    "question": [
+      { "text": "fahendrena", "answer": 0 },
+      { "text": "ny hazo", "answer": 0 },
+      { "text": "ny masoandro", "answer": 0 },
+      { "text": "ny faharetana", "answer": 1 }
+    ]
   },
   {
-    id: `ankamantatra-4`,
-    title: `Elobe tsy mipika`,
-    question: [
-      { text: `lanitra`, answer: 1 },
-      { text: `tafo`, answer: 0 },
-      { text: `rahona`, answer: 0 },
-      { text: `satroka`, answer: 0 },
-    ],
+    "id": "ankamantatra-4",
+    "title": "Izay rehetra tsy azo tsapain-tanana, fa hitanao?",
+    "question": [
+      { "text": "ny saina", "answer": 1 },
+      { "text": "ny maso", "answer": 0 },
+      { "text": "ny tanana", "answer": 0 },
+      { "text": "ny rivotra", "answer": 0 }
+    ]
   },
+  {
+    "id": "ankamantatra-5",
+    "title": "Inona no atao hoe 'manafina ny jiro'?",
+    "question": [
+      { "text": "ny fanamiana", "answer": 1 },
+      { "text": "ny akanjo", "answer": 0 },
+      { "text": "ny rivotra", "answer": 0 },
+      { "text": "ny fomba amam-panao", "answer": 0 }
+    ]
+  },
+  {
+    "id": "ankamantatra-6",
+    "title": "Izay mitranga, tsy miova?",
+    "question": [
+      { "text": "ny tany", "answer": 0 },
+      { "text": "ny masoandro", "answer": 1 },
+      { "text": "ny alina", "answer": 0 },
+      { "text": "ny fitiavana", "answer": 0 }
+    ]
+  },
+  {
+    "id": "ankamantatra-7",
+    "title": "Inona no miakatra, fa tsy midina?",
+    "question": [
+      { "text": "ny oram-panala", "answer": 1 },
+      { "text": "ny rano", "answer": 0 },
+      { "text": "ny habakabaka", "answer": 0 },
+      { "text": "ny fiainana", "answer": 0 }
+    ]
+  },
+  {
+    "id": "ankamantatra-8",
+    "title": "Inona no tsy hita, fa tsapa?",
+    "question": [
+      { "text": "ny fofona", "answer": 1 },
+      { "text": "ny maso", "answer": 0 },
+      { "text": "ny trano", "answer": 0 },
+      { "text": "ny rivotra", "answer": 0 }
+    ]
+  },
+  {
+    "id": "ankamantatra-9",
+    "title": "Izay miakatra nefa tsy midina?",
+    "question": [
+      { "text": "ny fitiavana", "answer": 1 },
+      { "text": "ny rivotra", "answer": 0 },
+      { "text": "ny lalana", "answer": 0 },
+      { "text": "ny fahatsiarovana", "answer": 0 }
+    ]
+  },
+  {
+    "id": "ankamantatra-10",
+    "title": "Inona no tsy afaka mahita fa afaka mihaino?",
+    "question": [
+      { "text": "ny feo", "answer": 1 },
+      { "text": "ny rano", "answer": 0 },
+      { "text": "ny maso", "answer": 0 },
+      { "text": "ny saina", "answer": 0 }
+    ]
+  },
+  {
+    "id": "ankamantatra-11",
+    "title": "Inona no mitranga fotsiny, fa tsy miseho?",
+    "question": [
+      { "text": "ny rivotra", "answer": 1 },
+      { "text": "ny lanitra", "answer": 0 },
+      { "text": "ny tanana", "answer": 0 },
+      { "text": "ny fahatsiarovana", "answer": 0 }
+    ]
+  },
+  {
+    "id": "ankamantatra-12",
+    "title": "Inona no mitovy amin'ny vato, fa tsy ho latsaka?",
+    "question": [
+      { "text": "ny vola", "answer": 1 },
+      { "text": "ny hazo", "answer": 0 },
+      { "text": "ny rano", "answer": 0 },
+      { "text": "ny ravina", "answer": 0 }
+    ]
+  },
+  {
+    "id": "ankamantatra-13",
+    "title": "Izay mitranga, fa tsy mitsangana?",
+    "question": [
+      { "text": "ny orana", "answer": 1 },
+      { "text": "ny trano", "answer": 0 },
+      { "text": "ny fo", "answer": 0 },
+      { "text": "ny aloka", "answer": 0 }
+    ]
+  },
+  {
+    "id": "ankamantatra-14",
+    "title": "Inona no mivaky, fa tsy manana hadalana?",
+    "question": [
+      { "text": "ny vato", "answer": 1 },
+      { "text": "ny masoandro", "answer": 0 },
+      { "text": "ny tany", "answer": 0 },
+      { "text": "ny hazo", "answer": 0 }
+    ]
+  },
+  {
+    "id": "ankamantatra-15",
+    "title": "Inona no tsy azo atao, fa tsy azonao amboarina?",
+    "question": [
+      { "text": "ny volana", "answer": 1 },
+      { "text": "ny trano", "answer": 0 },
+      { "text": "ny hazo", "answer": 0 },
+      { "text": "ny ranomainty", "answer": 0 }
+    ]
+  },
+  {
+    "id": "ankamantatra-16",
+    "title": "Inona no ho azo tsapain-tanana, fa tsy afaka jerena?",
+    "question": [
+      { "text": "ny fo", "answer": 1 },
+      { "text": "ny maso", "answer": 0 },
+      { "text": "ny tany", "answer": 0 },
+      { "text": "ny rivotra", "answer": 0 }
+    ]
+  },
+  {
+    "id": "ankamantatra-17",
+    "title": "Inona no mitranga, fa tsy ho avy?",
+    "question": [
+      { "text": "ny fitiavana", "answer": 1 },
+      { "text": "ny feo", "answer": 0 },
+      { "text": "ny maso", "answer": 0 },
+      { "text": "ny orana", "answer": 0 }
+    ]
+  },
+  {
+    "id": "ankamantatra-18",
+    "title": "Izay rehetra misy, fa tsy mahita?",
+    "question": [
+      { "text": "ny olona", "answer": 0 },
+      { "text": "ny fo", "answer": 1 },
+      { "text": "ny tany", "answer": 0 },
+      { "text": "ny volo", "answer": 0 }
+    ]
+  },
+  {
+    "id": "ankamantatra-19",
+    "title": "Inona no miavaka, fa tsy misakana?",
+    "question": [
+      { "text": "ny fitia", "answer": 1 },
+      { "text": "ny fitiavana", "answer": 0 },
+      { "text": "ny hazo", "answer": 0 },
+      { "text": "ny vato", "answer": 0 }
+    ]
+  },
+  {
+    "id": "ankamantatra-20",
+    "title": "Inona no mandefa fa tsy mila ny olona?",
+    "question": [
+      { "text": "ny rivotra", "answer": 1 },
+      { "text": "ny rano", "answer": 0 },
+      { "text": "ny tany", "answer": 0 },
+      { "text": "ny hazo", "answer": 0 }
+    ]
+  },
+  {
+    "id": "ankamantatra-21",
+    "title": "Inona no mihetsika fa tsy afaka miala?",
+    "question": [
+      { "text": "ny volana", "answer": 1 },
+      { "text": "ny masoandro", "answer": 0 },
+      { "text": "ny rano", "answer": 0 },
+      { "text": "ny tany", "answer": 0 }
+    ]
+  },
+  {
+    "id": "ankamantatra-22",
+    "title": "Inona no mahery, fa tsy afaka mitsangana?",
+    "question": [
+      { "text": "ny rano", "answer": 1 },
+      { "text": "ny vato", "answer": 0 },
+      { "text": "ny hazo", "answer": 0 },
+      { "text": "ny rivotra", "answer": 0 }
+    ]
+  },
+  {
+    "id": "ankamantatra-23",
+    "title": "Inona no mitovy amin'ny rivotra?",
+    "question": [
+      { "text": "ny fahatsiarovana", "answer": 1 },
+      { "text": "ny aloka", "answer": 0 },
+      { "text": "ny fo", "answer": 0 },
+      { "text": "ny tany", "answer": 0 }
+    ]
+  },
+  {
+    "id": "ankamantatra-24",
+    "title": "Inona no tsy mivaky fa mahazo mitsangana?",
+    "question": [
+      { "text": "ny vato", "answer": 1 },
+      { "text": "ny masoandro", "answer": 0 },
+      { "text": "ny tany", "answer": 0 },
+      { "text": "ny rivotra", "answer": 0 }
+    ]
+  },
+  {
+    "id": "ankamantatra-25",
+    "title": "Inona no manana vava, fa tsy afaka miteny?",
+    "question": [
+      { "text": "ny vokatra", "answer": 1 },
+      { "text": "ny vato", "answer": 0 },
+      { "text": "ny trano", "answer": 0 },
+      { "text": "ny feo", "answer": 0 }
+    ]
+  },
+  {
+    "id": "ankamantatra-26",
+    "title": "Izay mitombina, fa tsy mahatsapa?",
+    "question": [
+      { "text": "ny saina", "answer": 0 },
+      { "text": "ny fo", "answer": 1 },
+      { "text": "ny maso", "answer": 0 },
+      { "text": "ny tany", "answer": 0 }
+    ]
+  },
+  {
+    "id": "ankamantatra-27",
+    "title": "Inona no tsy azo tsapain-tanana, fa afaka mihaino?",
+    "question": [
+      { "text": "ny feo", "answer": 1 },
+      { "text": "ny maso", "answer": 0 },
+      { "text": "ny rivotra", "answer": 0 },
+      { "text": "ny vato", "answer": 0 }
+    ]
+  },
+  {
+    "id": "ankamantatra-28",
+    "title": "Inona no mitranga fa tsy hita?",
+    "question": [
+      { "text": "ny feo", "answer": 1 },
+      { "text": "ny ranomandry", "answer": 0 },
+      { "text": "ny vato", "answer": 0 },
+      { "text": "ny hazo", "answer": 0 }
+    ]
+  },
+  {
+    "id": "ankamantatra-29",
+    "title": "Inona no miakatra, nefa tsy midina?",
+    "question": [
+      { "text": "ny oram-panala", "answer": 1 },
+      { "text": "ny masoandro", "answer": 0 },
+      { "text": "ny rivotra", "answer": 0 },
+      { "text": "ny hazo", "answer": 0 }
+    ]
+  },
+  {
+    "id": "ankamantatra-30",
+    "title": "Inona no manao varotra, fa tsy mitady vola?",
+    "question": [
+      { "text": "ny rivotra", "answer": 0 },
+      { "text": "ny tonga lafatra", "answer": 1 },
+      { "text": "ny saka", "answer": 0 },
+      { "text": "ny omby", "answer": 0 }
+    ]
+  },
+  {
+    "id": "ankamantatra-31",
+    "title": "Iza no tsy afaka mijery, fa mivoaka amin'ny alina?",
+    "question": [
+      { "text": "ny volana", "answer": 1 },
+      { "text": "ny maso", "answer": 0 },
+      { "text": "ny rivotra", "answer": 0 },
+      { "text": "ny tany", "answer": 0 }
+    ]
+  },
+  {
+    "id": "ankamantatra-32",
+    "title": "Izay misokatra, fa tsy azo tsapain-tanana?",
+    "question": [
+      { "text": "ny fanantenana", "answer": 1 },
+      { "text": "ny tany", "answer": 0 },
+      { "text": "ny masoandro", "answer": 0 },
+      { "text": "ny volana", "answer": 0 }
+    ]
+  },
+  {
+    "id": "ankamantatra-33",
+    "title": "Inona no mikitika ny tany, fa tsy mihodina?",
+    "question": [
+      { "text": "ny hazo", "answer": 1 },
+      { "text": "ny rano", "answer": 0 },
+      { "text": "ny aloka", "answer": 0 },
+      { "text": "ny tany", "answer": 0 }
+    ]
+  },
+  {
+    "id": "ankamantatra-34",
+    "title": "Inona no mihodina, fa tsy mivaky?",
+    "question": [
+      { "text": "ny fanilo", "answer": 1 },
+      { "text": "ny fiara", "answer": 0 },
+      { "text": "ny varavarana", "answer": 0 },
+      { "text": "ny rivotra", "answer": 0 }
+    ]
+  },
+  {
+    "id": "ankamantatra-35",
+    "title": "Izay manana feno, fa tsy mahita?",
+    "question": [
+      { "text": "ny fo", "answer": 1 },
+      { "text": "ny maso", "answer": 0 },
+      { "text": "ny ranomainty", "answer": 0 },
+      { "text": "ny lanitra", "answer": 0 }
+    ]
+  },
+  {
+    "id": "ankamantatra-36",
+    "title": "Inona no tsy azo tsapain-tanana, fa afaka mitranga?",
+    "question": [
+      { "text": "ny fitiavana", "answer": 1 },
+      { "text": "ny rivotra", "answer": 0 },
+      { "text": "ny volana", "answer": 0 },
+      { "text": "ny rano", "answer": 0 }
+    ]
+  },
+  {
+    "id": "ankamantatra-37",
+    "title": "Inona no hita, fa tsy voamarina?",
+    "question": [
+      { "text": "ny fahatsiarovana", "answer": 1 },
+      { "text": "ny masoandro", "answer": 0 },
+      { "text": "ny tany", "answer": 0 },
+      { "text": "ny ranomandry", "answer": 0 }
+    ]
+  },
+  {
+    "id": "ankamantatra-38",
+    "title": "Inona no mitranga, fa tsy mahatsapa?",
+    "question": [
+      { "text": "ny fitiavana", "answer": 1 },
+      { "text": "ny tany", "answer": 0 },
+      { "text": "ny maso", "answer": 0 },
+      { "text": "ny feo", "answer": 0 }
+    ]
+  },
+  {
+    "id": "ankamantatra-39",
+    "title": "Inona no mitranga, fa tsy mivoaka?",
+    "question": [
+      { "text": "ny feo", "answer": 1 },
+      { "text": "ny fiara", "answer": 0 },
+      { "text": "ny rivotra", "answer": 0 },
+      { "text": "ny rano", "answer": 0 }
+    ]
+  },
+  {
+    "id": "ankamantatra-40",
+    "title": "Izay tsy azo atao amin'ny alina?",
+    "question": [
+      { "text": "mividy vary", "answer": 1 },
+      { "text": "manamboatra trano", "answer": 0 },
+      { "text": "manao diany", "answer": 0 },
+      { "text": "miasa", "answer": 0 }
+    ]
+  },
+  {
+    "id": "ankamantatra-41",
+    "title": "Inona no tsy mety raha tsy tonga fotoana?",
+    "question": [
+      { "text": "ny fitsangatsanganana", "answer": 1 },
+      { "text": "ny fety", "answer": 0 },
+      { "text": "ny varotra", "answer": 0 },
+      { "text": "ny teti-bola", "answer": 0 }
+    ]
+  },
+  {
+    "id": "ankamantatra-42",
+    "title": "Inona no tsy mety mivelatra amin'ny tany?",
+    "question": [
+      { "text": "ny rano", "answer": 1 },
+      { "text": "ny hazo", "answer": 0 },
+      { "text": "ny tany", "answer": 0 },
+      { "text": "ny ravina", "answer": 0 }
+    ]
+  },
+  {
+    "id": "ankamantatra-43",
+    "title": "Inona no manam-paharetana, fa tsy afaka mandositra?",
+    "question": [
+      { "text": "ny ranomandry", "answer": 1 },
+      { "text": "ny feo", "answer": 0 },
+      { "text": "ny tany", "answer": 0 },
+      { "text": "ny rivotra", "answer": 0 }
+    ]
+  },
+  {
+    "id": "ankamantatra-44",
+    "title": "Inona no mahafinaritra, fa tsy maharitra?",
+    "question": [
+      { "text": "ny fitiavana", "answer": 1 },
+      { "text": "ny rivotra", "answer": 0 },
+      { "text": "ny masoandro", "answer": 0 },
+      { "text": "ny volana", "answer": 0 }
+    ]
+  },
+  {
+    "id": "ankamantatra-45",
+    "title": "Inona no tsy manana fetra, fa azo tanterahina?",
+    "question": [
+      { "text": "ny feo", "answer": 1 },
+      { "text": "ny tany", "answer": 0 },
+      { "text": "ny rano", "answer": 0 },
+      { "text": "ny fo", "answer": 0 }
+    ]
+  },
+  {
+    "id": "ankamantatra-46",
+    "title": "Inona no miandry, fa tsy manan-kery?",
+    "question": [
+      { "text": "ny rano", "answer": 1 },
+      { "text": "ny rivotra", "answer": 0 },
+      { "text": "ny tany", "answer": 0 },
+      { "text": "ny volana", "answer": 0 }
+    ]
+  },
+  {
+    "id": "ankamantatra-47",
+    "title": "Inona no afaka manova, fa tsy ho latsaka?",
+    "question": [
+      { "text": "ny fanantenana", "answer": 1 },
+      { "text": "ny vola", "answer": 0 },
+      { "text": "ny tany", "answer": 0 },
+      { "text": "ny hazo", "answer": 0 }
+    ]
+  },
+  {
+    "id": "ankamantatra-48",
+    "title": "Izay tsy afaka mankany aminao, fa mitondra anao?",
+    "question": [
+      { "text": "ny fiara", "answer": 1 },
+      { "text": "ny rivotra", "answer": 0 },
+      { "text": "ny rano", "answer": 0 },
+      { "text": "ny varavarana", "answer": 0 }
+    ]
+  },
+  {
+    "id": "ankamantatra-49",
+    "title": "Inona no mihetsika, fa tsy mihaino?",
+    "question": [
+      { "text": "ny volana", "answer": 1 },
+      { "text": "ny rivotra", "answer": 0 },
+      { "text": "ny maso", "answer": 0 },
+      { "text": "ny tany", "answer": 0 }
+    ]
+  },
+  {
+    "id": "ankamantatra-50",
+    "title": "Inona no tsy azo tsapain-tanana, fa tsy manam-petra?",
+    "question": [
+      { "text": "ny rano", "answer": 1 },
+      { "text": "ny tany", "answer": 0 },
+      { "text": "ny hazo", "answer": 0 },
+      { "text": "ny volana", "answer": 0 }
+    ]
+  }
 ];
 
 const congrats = `arabaina!
